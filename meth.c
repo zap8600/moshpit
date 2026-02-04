145306a6dca9fc59944ab60a681a621f5aaf8dbe
zap8600 <zapost86@gmail.com>
2026-02-04 16:10:20 -0600

    wip im tired

---
+++ meth.c
@@ -0,0 +1,67 @@
+#include <stdint.h>
+
+// https://martin.kleppmann.com/papers/curve25519.pdf
+
+typedef int64_t field_element[16];
+
+// Turns a byte array into our element
+static void unpack_25519(field_element out, const uint8_t* in) {
+    for(uint8_t i = 0; i < 16; i++) {
+        out[i] = in[2 * i] + (((int64_t)in[(2*i) + 1]) << 8);
+    }
+    out[15] &= 0x7fff;
+}
+
+// Carry flag kinda thing
+static void carry_25519(field_element element) {
+    int64_t carry;
+    for(uint8_t i = 0; i < 16; i++) {
+        carry = element[i] >> 16;
+        element[i] -= carry << 16;
+        if(i < 15) {
+            element[i + 1] += carry;
+        } else {
+            element[0] += 38 * carry;
+        }
+    }
+}
+
+// Adds field elements
+static void field_add(field_element out, const field_element a, const field_element b) {
+    for(uint8_t i = 0; i < 16; i++) {
+        out[i] = a[i] + b[i];
+    }
+}
+
+// Subtracts field elements
+static void field_sub(field_element out, const field_element a, const field_element b) {
+    for(uint8_t i = 0; i < 16; i++) {
+        out[i] = a[i] - b[i];
+    }
+}
+
+static void field_mul(field_element out, const field_element a, const field_element b) {
+    uint8_t i;
+    uint8_t j;
+    int64_t product[31];
+
+    for(i = 0; i < 31; i++) {
+        product[i] = 0;
+    }
+
+    for(i = 0; i < 16; i++) {
+        for(j = 0; j < 16; j++) {
+            product[i + j] += a[i] * b[j];
+        }
+    }
+
+    for(i = 0; i < 15; i++) {
+        product[i] += 38 * product[i + 16];
+    }
+    for(i = 0; i < 16; i++) {
+        out[i] = product[i];
+    }
+
+    carry_25519(out);
+    carry_25519(out);
+}
