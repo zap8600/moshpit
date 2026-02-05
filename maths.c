4ea87a92b128ab31185eb1163015df051e02d21f
zap8600 <95487746+zap8600@users.noreply.github.com>
2026-02-05 15:26:53 +0000

    wip maths

---
+++ maths.c
@@ -0,0 +1,11 @@
+// Complicated math
+
+#include <stdint.h>
+
+// https://martin.kleppmann.com/papers/curve25519.pdf
+
+// Math notes:
+// Weird ∈ symbol: https://en.wikipedia.org/wiki/Element_of_a_set
+//
+
+// TODO: I need to learn to write ts on your own
