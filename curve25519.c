f21e6489996655bd3c5e27fc31146e28d50546c9
zap8600 <95487746+zap8600@users.noreply.github.com>
2026-02-09 17:45:37 +0000

    sigh

---
+++ curve25519.c
@@ -0,0 +1,12 @@
+// Math for calculating Curve 25519
+
+#include <stdint.h>
+
+// https://martin.kleppmann.com/papers/curve25519.pdf
+// https://datatracker.ietf.org/doc/html/rfc7748
+
+// Math notes:
+// Weird ∈ symbol: https://en.wikipedia.org/wiki/Element_of_a_set
+//
+
+// TODO: Figure this out because I'm not intelligent
